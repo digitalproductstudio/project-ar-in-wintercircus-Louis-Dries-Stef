c3c69456f49429f670bc932ff919a8341e953871
Louis Dierickx <dierickx.louis01@gmail.com>
2025-05-19 10:45:54 +0200

    pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
   base: "/project-ar-in-wintercircus-Louis-Dries-Stef/",
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
 });
