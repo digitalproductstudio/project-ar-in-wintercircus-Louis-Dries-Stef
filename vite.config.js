3fc0f68984e991b3bb18432e2630f951b3e38672
Louis Dierickx <dierickx.louis01@gmail.com>
2025-05-20 17:43:23 +0200

    vite config voor pages

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/project-ar-in-wintercircus-Louis-Dries-Stef/",
   plugins: [react()],
+  base: "/project-ar-in-wintercircus-Louis-Dries-Stef/",
   build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
+    outDir: "docs",
   },
 });
