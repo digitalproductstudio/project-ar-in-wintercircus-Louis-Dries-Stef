b4dc7099d9e9bf598334705ffe0858ce48696931
Stef Van Boven <stefvboven2020@outlook.com>
2025-06-04 13:39:26 +0200

    Added: 2nd cursor

--- src/components/BringingGame.jsx
+++ src/components/BringingGame.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { motion } from "framer-motion";
 
 function BringingGame({
@@ -21,7 +22,11 @@ function BringingGame({
           <motion.div
             key={item.id}
             className="absolute text-4xl"
-            style={{ top: item.y, left: item.x }}
+            style={{
+              top: item.y,
+              left: item.x,
+              cursor: `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='40' height='48' viewport='0 0 100 100' style='fill:black;font-size:24px;'><text y='50%'>🧺</text></svg>") 16 0, auto`,
+            }}
             onClick={() => collectItem(item.id)}
           >
             {item.emoji}
